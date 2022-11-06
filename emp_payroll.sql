c5697500c22a70d1784108603707c41ab6dc87d6
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-07 01:38:36 +0530

    Added: UC2 Created table employee_payroll

--- emp_payroll.sql
+++ emp_payroll.sql
@@ -1,3 +1,4 @@
+# UC1
 # To create a database.
 create database payroll_service;
 
@@ -6,3 +7,12 @@ show databases;
 
 # To use existing database.
 use payroll_service;
+
+# UC2
+# Creating a table with a auto-incremented column 'id'
+Create Table employee_payroll (
+	id int primary key auto_increment,
+    name varchar(25),
+    salary double,
+    start_date date
+);
